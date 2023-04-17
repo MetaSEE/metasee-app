31eda25ed7d2966fef031fae726d66ee29f1a32f
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-17 17:33:23 -0300

    env prod updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   DIR_APP_THUMBS_AVATAR: "assets/images/thumbs_avatar/",
+  // URL_API: "http://localhost:3333/",
   URL_API: "https://metasee-api.onrender.com/",
   URL_VIRTUALWORLD: "http://localhost:3321/",
   DIR_VW_THUMBS: "images/thumbs/"
