e37f72cdf930574d1a00ec2e14b88f1f108389e2
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-18 08:58:35 -0300

    some changes to deploy in heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   DIR_APP_THUMBS_AVATAR: "assets/images/thumbs_avatar/",
-  URL_API: "https://bcfh5v7dz4.execute-api.sa-east-1.amazonaws.com/master/",
+  URL_API: "https://metasee-api.herokuapp.com/",
   URL_VIRTUALWORLD: "http://localhost:3321/",
   DIR_VW_THUMBS: "images/thumbs/"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   DIR_APP_THUMBS_AVATAR: "assets/images/thumbs_avatar/",
   // URL_API: "http://localhost:3333/",
-  URL_API: "https://bcfh5v7dz4.execute-api.sa-east-1.amazonaws.com/master/",
+  URL_API: "https://metasee-api.herokuapp.com/",
   URL_VIRTUALWORLD: "http://localhost:3321/",
   DIR_VW_THUMBS: "images/thumbs/"
 };
